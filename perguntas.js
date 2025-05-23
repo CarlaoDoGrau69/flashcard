a75d7d1b95fe0ed0f66a515f9b579047e701a9be
CarlaoDoGrau69 <carlos.fontana.neto@escola.pr.gov.br>
2025-05-23 10:43:26 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Programação',
+    'O que uma variavel?',
+    'É um determinador de estado'
+)
+
+criaCartao(
+    'Geografia',
+    'O que são as linhas geográficas?',
+    'Linhass imaginarias que separam países, estados e cidades'
+)
+
+criaCartao(
+    'Matematica',
+    'Quanto é 0:0?',
+    'Impossível de se calcular'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz bois em Inglês?',
+    'boi em ingles é oxes (RAI)'
+)
